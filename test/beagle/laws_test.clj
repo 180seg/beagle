d03f887c96d5d9808972916b49a532edd3eeb8e7
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-09-21 17:25:52 -0300

    add tests and workflow to run tests

---
+++ test/beagle/laws_test.clj
@@ -0,0 +1,15 @@
+(ns beagle.laws-test
+  (:require 
+    [beagle.laws :as laws]
+    [clojure.test :refer :all]))
+
+(deftest ->simulate-custom-policy
+  (testing "when valid laws are provided."
+    (let [expected-output {:ActionNames [ "iam:*"], :ResourceArns ["*"]}
+          laws [{:Statement [{:Action ["iam:*"] :Resource ["*"]}]}]]
+      (is (= expected-output (laws/->simulate-custom-policy laws)))))
+
+  (testing "when not valid laws are provided."
+    (let [expected-output {:ActionNames [] , :ResourceArns []}
+          laws [{:Statement [{:Actions ["iam:*"] :Resources ["*"]}]}]]
+      (is (= expected-output (laws/->simulate-custom-policy laws))))))
